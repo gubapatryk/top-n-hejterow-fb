5a08f2fc5a6c4a7be21703bda715c8f98d97d8c9
Max <maxymilian.kowalski@codility.com>
2021-11-21 20:14:02 +0100

    log messages and new cookie files

--- TopFacebookHaters/scraper.py
+++ TopFacebookHaters/scraper.py
@@ -1,21 +1,35 @@
 ###################################################################
 #               Extracting the data from website                  #
 ###################################################################
+import logging
 
 from facebook_scraper import get_posts
 
 from azure.ai.textanalytics import TextAnalyticsClient
 from azure.core.credentials import AzureKeyCredential
 
+cookie_files = ["cookies1.txt", "cookies2.txt", "cookies3.txt"]
 
 def scrape(name = 'cnn', _pages = 1):
     #name of facebook page
+
+    logging.info('Succesfully started scrape function.')
 
     result = ""
 
     people = {}
 
-    for post in get_posts(name, pages = _pages, options = {"comments": True}, cookies = 'cookies.txt'):#cridentials = ('mvboevfegkjaktnqlq@sdvrecft.com', 'jadro123'),
+    posts = {}
+    try:
+        # if accounts gets banned then just change to other cookies file
+        posts = get_posts(name, pages = _pages, options = {"comments": True}, cookies = cookie_files[0])
+    except:
+        logging.error('Internal scraper error, account might be banned.')
+        return "Internal scraper error, account might be banned."
+
+    logging.info('Succesfully scraped comments.')
+
+    for post in posts:
         for comment in post['comments_full']:
             #original commenter name
             op_name = comment['commenter_name']
@@ -28,7 +42,7 @@ def scrape(name = 'cnn', _pages = 1):
             for reply in comment['replies']:
                 #replier name
                 re_name = reply['commenter_name']
-                re_text = reply['comment_text']#.encode()
+                re_text = reply['comment_text']
                 if re_text != 'None':
                     if re_name in people:
                         people[re_name].append(re_text)
@@ -44,7 +58,7 @@ def scrape(name = 'cnn', _pages = 1):
     #       Analyzing the comments using Azure Cognitive Services     #
     ###################################################################
 
-    key = ""
+    key = "03b61cf2571e499185c3795834dd94d8"
     endpoint = "https://comment-analyzer.cognitiveservices.azure.com/"
 
 
@@ -78,7 +92,7 @@ def scrape(name = 'cnn', _pages = 1):
     result += "\noverall there are " + str(count) + " negative comments"
 
     negative_comments.sort(reverse = True)
-
+    logging.info('Succesfully analyzed comments.')
     for hater in range(min(5, len(negative_comments))):
         result += '\n' + negative_comments[hater][1] + " made " + str(negative_comments[hater][0]) + " negative comments, first 5: " + str(negative_comments[hater][2][:5])
     return result
